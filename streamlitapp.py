c5d8c60512e118939a5bad859abaed5c972bd0ee
Azazel0203 <aadarshkr.singh.cd.ece21@itbhu.ac.in>
2024-03-06 17:19:25 +0530

    put in password

--- streamlitapp.py
+++ streamlitapp.py
@@ -3,6 +3,7 @@ import json
 import traceback
 import pandas as pd
 from dotenv import load_dotenv
+load_dotenv()
 # import langchain
 from src.mcq_generator.utils import read_file, get_table_data
 from src.mcq_generator.logger import logging
@@ -19,6 +20,7 @@ with open('Response.json', 'r') as file:
 
 # STREAMLIT PART
 
+st_key = os.getenv("ST_KEY")
 # the title
 st.title("MCQ Generator")
 
@@ -30,45 +32,51 @@ with st.form("user_inputs"):
     mcq_count=st.number_input("No of Questions", min_value=3, max_value=50)
     subject = st.text_input("Insert the Subject", max_chars=20)
     tone=st.text_input("Complexity level of Questions", max_chars=20, placeholder="Simple")
+    input_key = st.text_input("Enter the secret key", max_chars=50, placeholder="Password")
+    print(input_key)
+    print(st_key)
     button = st.form_submit_button("Create MCQs")
     
     # check for button click
     if button and uploaded_file is not None and mcq_count and subject and tone:
-        with st.spinner("Loading..."):
-            try:
-                text = read_file(uploaded_file)
-                # call the llm while getting the token metrics
-                with get_openai_callback() as cb:
-                    response=generate_and_evaluate_quiz(
-                        {
-                            "text": text,
-                            "number": mcq_count,
-                            "subject": subject,
-                            "tone": tone,
-                            "response_json": json.dumps(RESPONSE_JSON) 
-                        }
-                    )
-            except Exception as e:
-                traceback.print_exception(type(e), e, e.__traceback__)
-                st.error("Error")
-            
-            else:
-                print (f"Total Tokens: {cb.total_tokens}")
-                print (f"Prompt Tokens: {cb.prompt_tokens}")    
-                print (f"Completion Tokens: {cb.completion_tokens}")
-                print (f"Total Cost: {cb.total_cost}")
-                if isinstance(response, dict):
-                    # extract the quiz data from the response
-                    quiz = response.get("quiz", None)
-                    if quiz is not None:
-                        table_data=get_table_data(quiz)
-                        if table_data is not None:
-                            df = pd.DataFrame(table_data)
-                            df.index=df.index+1
-                            st.table(df)
-                            # for review
-                            st.text_area(label="Review", value=response["review"])
-                        else:
-                            st.error("Error in the table data")
+        if input_key == st_key:
+            with st.spinner("Loading..."):
+                try:
+                    text = read_file(uploaded_file)
+                    # call the llm while getting the token metrics
+                    with get_openai_callback() as cb:
+                        response=generate_and_evaluate_quiz(
+                            {
+                                "text": text,
+                                "number": mcq_count,
+                                "subject": subject,
+                                "tone": tone,
+                                "response_json": json.dumps(RESPONSE_JSON) 
+                            }
+                        )
+                except Exception as e:
+                    traceback.print_exception(type(e), e, e.__traceback__)
+                    st.error("Error")
+
                 else:
-                    st.write(response)
+                    print (f"Total Tokens: {cb.total_tokens}")
+                    print (f"Prompt Tokens: {cb.prompt_tokens}")    
+                    print (f"Completion Tokens: {cb.completion_tokens}")
+                    print (f"Total Cost: {cb.total_cost}")
+                    if isinstance(response, dict):
+                        # extract the quiz data from the response
+                        quiz = response.get("quiz", None)
+                        if quiz is not None:
+                            table_data=get_table_data(quiz)
+                            if table_data is not None:
+                                df = pd.DataFrame(table_data)
+                                df.index=df.index+1
+                                st.table(df)
+                                # for review
+                                st.text_area(label="Review", value=response["review"])
+                            else:
+                                st.error("Error in the table data")
+                    else:
+                        st.write(response)
+        else:
+            st.error("Wrong Password")
